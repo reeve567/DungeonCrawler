80ee484489f45f7e1f7919631c20268b1adc44f6
Reeve <reeveblake@gmail.com>
2020-03-01 18:48:42 -0500

    *

--- src/main/kotlin/dev/dungeoncrawler/dungeon/PrefabManager.kt
+++ src/main/kotlin/dev/dungeoncrawler/dungeon/PrefabManager.kt
@@ -3,44 +3,36 @@ package dev.dungeoncrawler.dungeon
 object PrefabManager {
 	val prefabList = ArrayList<Prefab>()
 	val empty = Prefab(999, 999, PrefabDirectionalInformation(false, false, false, false))
-	val covered = Prefab(998, 999, PrefabDirectionalInformation(false, false, false, false))
-	val checkpoint = Prefab(998, 998, PrefabDirectionalInformation(true, true, true, true))
+	val covered = Prefab(1000, 1001, PrefabDirectionalInformation(false, false, false, false))
+	val checkpoint = Prefab(1003, 1000, PrefabDirectionalInformation(true, true, true, true))
 	
 	init {
 		prefabList.addAll(
 				listOf(
 						// full intersection
 						Prefab(1000, 1000, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1001, 1000, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1002, 1000, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1000, 1001, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1001, 1001, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1002, 1001, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1000, 1002, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1001, 1002, PrefabDirectionalInformation(true, true, true, true)),
-						Prefab(1002, 1002, PrefabDirectionalInformation(true, true, true, true)),
 						
 						// hallway
-						Prefab(1000, 1003, PrefabDirectionalInformation(false, false, true, true)),
-						Prefab(1001, 1003, PrefabDirectionalInformation(true, true, false, false)),
+						Prefab(1001, 1000, PrefabDirectionalInformation(true, true, false, false)),
+						Prefab(1002, 1000, PrefabDirectionalInformation(false, false, true, true)),
 						
 						// dead end
-						Prefab(1000, 1005, PrefabDirectionalInformation(true, false, false, false)),
-						Prefab(1001, 1005, PrefabDirectionalInformation(false, false, true, false)),
-						Prefab(1002, 1005, PrefabDirectionalInformation(false, true, false, false)),
-						Prefab(1003, 1005, PrefabDirectionalInformation(false, false, false, true)),
+						Prefab(1000, 1006, PrefabDirectionalInformation(true, false, false, false)),
+						Prefab(1001, 1006, PrefabDirectionalInformation(false, false, true, false)),
+						Prefab(1002, 1006, PrefabDirectionalInformation(false, true, false, false)),
+						Prefab(1003, 1006, PrefabDirectionalInformation(false, false, false, true)),
 						
 						// three way
-						Prefab(1000, 1007, PrefabDirectionalInformation(true, false, true, true)),
-						Prefab(1001, 1007, PrefabDirectionalInformation(true, true, true, false)),
-						Prefab(1002, 1007, PrefabDirectionalInformation(false,true, true, true)),
-						Prefab(1003, 1007, PrefabDirectionalInformation(true, true, false, true)),
+						Prefab(1000, 1002, PrefabDirectionalInformation(true, true, true, false)),
+						Prefab(1001, 1002, PrefabDirectionalInformation(false, true, true, true)),
+						Prefab(1002, 1002, PrefabDirectionalInformation(true, true, false, true)),
+						Prefab(1003, 1002, PrefabDirectionalInformation(true, false, true, true)),
 						
 						//corner
-						Prefab(1000, 1009, PrefabDirectionalInformation(true,false,false,true)),
-						Prefab(1001, 1009, PrefabDirectionalInformation(true,false,true,false)),
-						Prefab(1002, 1009, PrefabDirectionalInformation(false,true,true,false)),
-						Prefab(1003, 1009, PrefabDirectionalInformation(false,true,false,true)),
+						Prefab(1000, 1004, PrefabDirectionalInformation(true, false, true, false)),
+						Prefab(1001, 1004, PrefabDirectionalInformation(false, true, true, false)),
+						Prefab(1002, 1004, PrefabDirectionalInformation(false, true, false, true)),
+						Prefab(1003, 1004, PrefabDirectionalInformation(true, false, false, true)),
 						
 						// none
 						covered
